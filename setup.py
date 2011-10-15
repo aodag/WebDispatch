acda71446edf84180e0623a4a05fdedcd4fd85ac
Atsushi Odagiri <aodagx@gmail.com>
2011-10-15 03:02:25 -0700

    garden

--- setup.py
+++ setup.py
@@ -13,4 +13,17 @@ setup(
     test_suite="webdispatch",
     license="MIT",
     install_requires=requires,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: WSGI',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Middleware',
+        'Topic :: Software Development :: Libraries',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+    ],
 )
