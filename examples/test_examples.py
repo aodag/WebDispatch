b576d59f0efba2da72ca6a56a73cd65c4a2aa386
Atsushi Odagiri <aodagx@gmail.com>
2011-10-15 01:54:05 -0700

    example for view of webob wsgify

--- examples/test_examples.py
+++ examples/test_examples.py
@@ -14,6 +14,19 @@ class HelloTests(unittest.TestCase):
         res = app.get('/hello/aodag')
         self.assertTrue('Hello aodag' in res)
 
+class WebObHelloTests(unittest.TestCase):
+    def _getAUT(self):
+        from webobdispatcher import app
+        return webtest.TestApp(app)
+
+    def test_it(self):
+        app = self._getAUT()
+        res = app.get('/')
+        self.assertTrue('Hello' in res)
+
+        res = app.get('/hello/aodag')
+        self.assertTrue('Hello aodag' in res)
+
 class SubroutingTests(unittest.TestCase):
 
     def _getAUT(self):
---
+++ examples/webobdispatcher.py
@@ -0,0 +1,29 @@
+from webdispatch import Dispatcher
+from webob import Request
+from webob.dec import wsgify
+from webdispatch.mixins import URLMapperMixin
+
+class MyRequest(Request, URLMapperMixin):
+    pass
+
+class WebObDispatcher(Dispatcher):
+    def add_url(self, name, pattern, view):
+        application = wsgify(view, RequestClass=MyRequest)
+        super(WebObDispatcher, self).add_url(name, pattern, application)
+
+def index(request):
+    url = request.generate_url("hello", xname="webdispatch")
+    return '<a href="%s">Hello</a>' % (url,)
+
+def hello(request):
+    return "Hello %s" % request.urlvars['xname']
+
+app = WebObDispatcher()
+app.add_url('home', '/', index)
+app.add_url('hello', '/hello/{xname}', hello)
+
+if __name__ == '__main__':
+    from wsgiref.simple_server import make_server
+
+    httpd = make_server('0.0.0.0', 8080, app)
+    httpd.serve_forever()
