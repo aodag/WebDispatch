9f56efb04c2e493ac110e484ea5c7ab8ed022774
Atsushi Odagiri <aodagx@gmail.com>
2011-10-17 01:41:42 +0900

    garden

--- setup.py
+++ setup.py
@@ -1,18 +1,37 @@
 from setuptools import setup, find_packages
 
 requires = []
+import os
 
 try:
     import collections.OrderedDict
 except ImportError:
     requires.append('ordereddict')
 
+here = os.path.dirname(__file__)
+readme = None
+changes = None
+
+def _read(name):
+    try:
+        return open(os.path.join(here, name)).read()
+    except:
+        return None
+
+readme = _read("README.txt")
+changes = _read("CHANGES.txt")
+
 setup(
     name="WebDispatch",
+    author="Atsushi Odagiri",
+    author_email="aodagx@gmail.com",
+    description="dispatch request on wsgi application.",
+    long_description=readme + "\n" + changes,
     version="0.0",
     test_suite="webdispatch",
     license="MIT",
     install_requires=requires,
+    url='http://github.com/aodag/WebDispatch',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Web Environment',
