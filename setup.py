7d399cdf40fcd6adb24b08fdb8f5e0b9330a5d81
Atsushi Odagiri <aodagx@gmail.com>
2013-09-02 21:36:48 +0900

    prep for 1.0

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 from setuptools import setup, find_packages
 
 requires = []
+tests_require = [
+    "nose",
+    "coverage",
+]
 import os
 
 try:
@@ -33,14 +37,15 @@ setup(
     author_email="aodagx@gmail.com",
     description="dispatch request on wsgi application.",
     long_description=readme + "\n" + changes,
-    version="1.0b5",
+    version="1.0",
     test_suite="webdispatch",
     license="MIT",
     install_requires=requires,
+    tests_require=tests_require,
     url='http://github.com/aodag/WebDispatch',
     packages=find_packages(),
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
@@ -48,9 +53,10 @@ setup(
         'Topic :: Internet :: WWW/HTTP :: WSGI',
         'Topic :: Internet :: WWW/HTTP :: WSGI :: Middleware',
         'Topic :: Software Development :: Libraries',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
     ],
     entry_points=points,
 )
